7e7d120aa6cad34960cd07e7cd7932e0e604a42c
Callum Flack <callum@callumflack.design>
2024-09-11 12:53:28 +1000

    apps/web add writing index page

---
+++ apps/web/src/app/writing/layout.tsx
@@ -0,0 +1,21 @@
+const title = `Writing — Callum Flack Design & Development`;
+const description = "An archive of projects, writing and ideas.";
+
+export const generateMetadata = () => {
+  return {
+    title,
+    description,
+    twitter: {
+      title,
+      description,
+    },
+  };
+};
+
+export default function PostLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
---
+++ apps/web/src/app/writing/page.tsx
@@ -0,0 +1,40 @@
+import { Text } from "@repo/ui/text";
+import { PageWrapper } from "@/components/page";
+import { TitleHeader } from "@/components/elements";
+import { PagePostsIndexClient } from "@/components/page/page-posts-index-client";
+import type { PostsKind } from "@/components/post";
+import config from "src/config";
+
+type SearchParams = Record<string, string | string[] | undefined>;
+
+export default function WritingIndexPage({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const kind: PostsKind = "writing";
+  const currentSort = (searchParams.sort as string) || kind;
+
+  const heading = (
+    <div className="space-y-2 text-balance lg:w-11/12">
+      <Text as="h1" intent="title">
+        If you have the words, you’ll find the way.
+      </Text>
+      <Text dim>
+        Writing about creativity, design and complexity through the lens of
+        attention, interfaces and systems composition. Signup for new posts{" "}
+        <a className="link" href={config.SUBSTACK_URL}>
+          here
+        </a>
+        .
+      </Text>
+    </div>
+  );
+
+  return (
+    <PageWrapper>
+      <TitleHeader>{heading}</TitleHeader>
+      <PagePostsIndexClient initialSort={currentSort} kind={kind} />
+    </PageWrapper>
+  );
+}
