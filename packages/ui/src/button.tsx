6211acc65771f990483566ccd589b220bcdff19f
Callum Flack <callum@callumflack.design>
2024-09-11 17:47:12 +1000

    class-variance-authority > cva

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { cx, cva, type VariantProps } from "class-variance-authority";
+import { cx, cva, type VariantProps } from "cva";
 
-const buttonVariants = cva(
-  [
+const buttonVariants = cva({
+  base: [
     "inline-flex items-center justify-center gap-1 whitespace-nowrap",
     "font-medium leading-none",
     "transition-colors",
@@ -13,63 +13,58 @@ const buttonVariants = cva(
     "disabled:pointer-events-none disabled:opacity-50",
     // 'translate-y-[-0.03em] transform',
   ],
-  {
-    variants: {
-      variant: {
-        default: "bg-fill-tint hover:bg-fill text-canvas",
-        accent: "bg-accent hover:bg-fill text-canvas",
-        reverse: "bg-background hover:bg-background text-fill",
-        outline: "border border-line hover:border-solid-hover text-fill", // bg-[rgba(255,255,255,0.2)]
-        destructive: "bg-destructive text-canvas hover:bg-destructive/90",
-        ghost: "hover:bg-background-hover hover:text-solid",
-        link: "text-fill underline-offset-4 hover:underline link",
-        icon: [
-          "hover:bg-background-hover hover:text-fill",
-          " hover:!text-fill",
-        ],
-      },
-      size: {
-        default: "h-[44px] px-w6",
-        sm: "h-[38px] px-w4 text-medium",
-        lg: "h-11 rounded-md px-8",
-        icon: "size-[42px] ",
-      },
-      // These must match text.tsx
-      color: {
-        default: "inherit !border-current",
-        fill: "text-fill",
-        solid: "text-solid border-solid",
-        background: "text-background border-background",
-        canvas: "text-canvas border-canvas",
-        accent: "text-accent border-accent",
-        // other semantics, maybe?
-        // subtle: "text-primary/50",
-        // notice: "text-notice",
-        // contrast: "text-contrast/90",
-      },
-      caps: {
-        true: "uppercase tracking-meta",
-      },
-      full: {
-        true: "w-full",
-      },
+  variants: {
+    variant: {
+      default: "bg-fill-tint hover:bg-fill text-canvas",
+      accent: "bg-accent hover:bg-fill text-canvas",
+      reverse: "bg-background hover:bg-background text-fill",
+      outline: "border border-line hover:border-solid-hover text-fill", // bg-[rgba(255,255,255,0.2)]
+      destructive: "bg-destructive text-canvas hover:bg-destructive/90",
+      ghost: "hover:bg-background-hover hover:text-solid",
+      link: "text-fill underline-offset-4 hover:underline link",
+      icon: ["hover:bg-background-hover hover:text-fill", " hover:!text-fill"],
     },
-    compoundVariants: [
-      // Apply classes when lead size and bold weight
-      // Applied thus `button({ size: "lead", serif })`
-      {
-        variant: ["icon"],
-        size: "icon",
-        class: "rounded-[13px] [&_svg]:size-[1.125em]",
-      },
-    ],
-    defaultVariants: {
-      // If I turn these on, these styles are rendered in EVERY use of Button. My understanding is that if overriden by another prop value, they are not applied (which is handled by cn). WTJF?
-      // variant: "default",
-      size: "default",
+    size: {
+      default: "h-[44px] px-w6",
+      sm: "h-[38px] px-w4 text-medium",
+      lg: "h-11 rounded-md px-8",
+      icon: "size-[42px] ",
     },
-  }
-);
+    // These must match text.tsx
+    color: {
+      default: "inherit !border-current",
+      fill: "text-fill",
+      solid: "text-solid border-solid",
+      background: "text-background border-background",
+      canvas: "text-canvas border-canvas",
+      accent: "text-accent border-accent",
+      // other semantics, maybe?
+      // subtle: "text-primary/50",
+      // notice: "text-notice",
+      // contrast: "text-contrast/90",
+    },
+    caps: {
+      true: "uppercase tracking-meta",
+    },
+    full: {
+      true: "w-full",
+    },
+  },
+  compoundVariants: [
+    // Apply classes when conditions are met
+    // Applied thus `button({ size: "lead", serif })`
+    {
+      variant: ["icon"],
+      size: "icon",
+      class: "rounded-[13px] [&_svg]:size-[1.125em]",
+    },
+  ],
+  defaultVariants: {
+    // If I turn these on, these styles are rendered in EVERY use of Button. My understanding is that if overriden by another prop value, they are not applied (which is handled by cn). WTJF?
+    // variant: "default",
+    size: "default",
+  },
+});
 
 export interface ButtonProps
   extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "color">,
